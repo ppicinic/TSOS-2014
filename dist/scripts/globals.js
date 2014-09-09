34b3de283d527efdfefaf2d4058ee2c270cea214
Phil Picinic <philpicinic@gmail.com>
2014-09-09 16:48:23 -0400

    added files - not sure what they are... webstorm or gulp?

--- dist/scripts/globals.js
+++ dist/scripts/globals.js
@@ -9,7 +9,7 @@ Operating System Concepts 8th edition by Silberschatz, Galvin, and Gagne.  ISBN 
 // Global "CONSTANTS" (There is currently no const or final or readonly type annotation in TypeScript.)
 // TODO: Make a global object and use that instead of the "_" naming convention in the global namespace.
 //
-var APP_NAME = "TSOS";
+var APP_NAME = "";
 var APP_VERSION = "0.07";
 
 var CPU_CLOCK_INTERVAL = 100;
